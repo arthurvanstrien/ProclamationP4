638961504a46761cdcebac60d60a61ce35853fc3
LAPTOP-PO4KSU0T\Gijs <gcmfesse@avans.nl>
2017-06-15 15:04:35 +0200

    Laatste pop-up

--- app/src/main/java/com/aftersoft/projecthawksnest/GalleryActivity.java
+++ app/src/main/java/com/aftersoft/projecthawksnest/GalleryActivity.java
@@ -1,10 +1,13 @@
 package com.aftersoft.projecthawksnest;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,6 +20,15 @@ public class GalleryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final AlertDialog.Builder builder1 = new AlertDialog.Builder(GalleryActivity.this);
+        builder1.setMessage("Press and hold a photo to enlarge it.");
+        builder1.setNeutralButton("I understand", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder1.show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
 
